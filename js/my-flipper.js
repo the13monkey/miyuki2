11299d60ca414d6ae56b5e0377c54802b3adc1e5
Dinah Chen <dinah.chen.ccy@gmail.com>
2021-03-25 16:24:24 -0700

    upload 3

---
+++ js/my-flipper.js
@@ -0,0 +1,9 @@
+let next = document.querySelector("#next");
+
+next.addEventListener("click", () => {
+
+    let enWrapper = document.querySelector("#en-wrapper");
+
+    enWrapper.classList.add("flipping"); 
+
+});
